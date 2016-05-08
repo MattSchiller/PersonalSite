5144dd19079e494239c9bf2fa3674f3f0b38a45e
Matt Schiller <matt.s.schiller@gmail.com>
2016-05-07 22:16:11 -0400

    Initial commit

---
+++ src/scripts/main.jsx
@@ -0,0 +1,33 @@
+var React   = require('react');
+var SimType = require('./simType.jsx');
+//var Menu    = require('./menu.jsx');
+
+var App = React.createClass({
+  
+  render: function() {
+    let content = "~Cindent0~" + "function " + "~Cfunc~" + "getModuleName" + "~CfuncName~" + "() {"  + "~l0~"
+    + "~p350~" + "~Cindent1~"
+                    + "var" + "~Cfunc~" + " name   " + "~ckey~" + "= " + "~q+~" + "~p350~" + "simType.jsx" + "~q-~" + ";" + "~l0~"
+    + "~p350~" + "~Cindent1~"
+                    + "var" + "~Cfunc~" + " author " + "~ckey~" + "= " + "~q+~" + "~p350~" + "Matt Schiller (c) 1987" + "~p500~" + "~b4~" + "2016" + "~q-~" + ";" + "~l0~"
+                + "~Cindent0~" + "}" + "~l0~"
+                + "~Cindent0~" + " " + "~l0~" //Dummy line
+                + "~Cindent0~" + "function " + "~Cfunc~" + "getContactInfo" + "~CfuncName~" + "() {"  + "~l0~"
+    + "~p350~" + "~Cindent1~"
+                    + "var" + "~Cfunc~" + " email " + "~ckey~" + "= " + "~q+~" + "~p350~" + "matt.s.schiller@gmail.com" + "~q-~" + ";" + "~l0~"
+                + "~Cindent0~" + "}";
+                
+    return (
+      <div>
+        <SimType content = { content } />
+      </div>
+      )
+
+  }
+
+});
+//**********************************Page initialization
+ReactDOM.render(
+  <App />,
+  document.getElementById('content')
+);
---
+++ src/scripts/typedBucket.js
@@ -0,0 +1,6 @@
+var TypedBucket = function(text = "", className = "") {
+  this.text = text;
+  this.className = className;
+};
+
+module.exports = TypedBucket;
