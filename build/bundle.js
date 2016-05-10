0222da2aa72b8e3e6fa195709c9428fa48521f56
Matt Schiller <matt.s.schiller@gmail.com>
2016-05-10 19:46:52 -0400

    added resume, contact page, and more formatting

--- build/bundle.js
+++ build/bundle.js
@@ -20196,9 +20196,9 @@
 	"use strict";
 
 	var Hobbies = {
-	    stub: "~Cindent0~" + "~ccomment~/* So you're interested to know what sort of things I like to get up to in my free time, eh?" + "~l0~" + "~Cindent0~" + "~l0~" + "~Cindent0~" + "~ccomment~ Well, I'm happy to oblige! Feel free to explore the links below to take you to the relevant albums. */" + "~l0~" + "~Cindent0~" + "~l0~",
+	      stub: "~Cindent0~" + "~ccomment~/* So you're interested to know what sort of things I like to get up to in my free time, eh?" + "~l0~" + "~Cindent0~" + "~l0~" + "~Cindent0~" + "~ccomment~ Well, I'm happy to oblige! Feel free to explore the links below to take you to the relevant albums. */" + "~l0~" + "~Cindent0~" + "~l0~",
 
-	    writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~p350~" + "~Cindent1~" + "origami" + "~Cfunc~" + ": " + "~c0~" + "24 years" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
+	      writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~" + "24 years" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~" + "www.MostlyNonFiction.com" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~" + "super-cool" + "~ahttps://www.google.com/search?q=costumes&rlz=1CAACAG_enUS674US674&oq=costumes&aqs=chrome..69i57j0l5.895j0j7&sourceid=chrome&ie=UTF-8~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
 	};
 
 	module.exports = Hobbies;
--- src/scripts/pages/hobbies.js
+++ src/scripts/pages/hobbies.js
@@ -5,11 +5,15 @@ var Hobbies = {
         + "~Cindent0~" + "~l0~"
         
     , writing:  "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~"
-   + "~p350~" + "~Cindent1~"
-                  + "origami" + "~Cfunc~" + ": " + "~c0~"
-                    + "24 years"
-                           + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";"
-                      + "~l0~"
+              + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~"
+                    + "24 years" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";"
+                    + "~l0~"
+              + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~"
+                    + "www.MostlyNonFiction.com" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";"
+                    + "~l0~"
+              + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~"
+                    + "super-cool" + "~ahttps://www.google.com/search?q=costumes&rlz=1CAACAG_enUS674US674&oq=costumes&aqs=chrome..69i57j0l5.895j0j7&sourceid=chrome&ie=UTF-8~" + "~c0~" + ";"
+                    + "~l0~"
               + "~Cindent0~" + "}"
 };
 
