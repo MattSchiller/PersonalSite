f950905a2270df38053d3bfa0ee57a5692b0db75
Matt Schiller <matt.s.schiller@gmail.com>
2016-05-10 20:24:24 -0400

    Updated album links

--- build/bundle.js
+++ build/bundle.js
@@ -20198,7 +20198,7 @@
 	var Hobbies = {
 	      stub: "~Cindent0~" + "~ccomment~/* So you're interested to know what sort of things I like to get up to in my free time, eh?" + "~l0~" + "~Cindent0~" + "~l0~" + "~Cindent0~" + "~ccomment~ Well, I'm happy to oblige! Feel free to explore the links below to take you to the relevant albums. */" + "~l0~" + "~Cindent0~" + "~l0~",
 
-	      writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~" + "24 years" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~" + "www.MostlyNonFiction.com" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~" + "super-cool" + "~ahttps://www.google.com/search?q=costumes&rlz=1CAACAG_enUS674US674&oq=costumes&aqs=chrome..69i57j0l5.895j0j7&sourceid=chrome&ie=UTF-8~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
+	      writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~" + "24 years" + "~ahttps://goo.gl/photos/dxTN5bMiAVqE5gA89~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~" + "www.MostlyNonFiction.com" + "~ahttps://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=origami~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~" + "super-cool" + "~ahttps://goo.gl/photos/YKGpuzTFbfbqWzKz6~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
 	};
 
 	module.exports = Hobbies;
