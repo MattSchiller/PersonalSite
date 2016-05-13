aba85cf11c0c2827bf99ed7f2e8e1e49b0b6fbc8
Matt Schiller <matt.s.schiller@gmail.com>
2016-05-13 00:00:17 -0400

    fixed links

--- src/scripts/pages/hobbies.js
+++ src/scripts/pages/hobbies.js
@@ -9,7 +9,7 @@ var Hobbies = {
                     + "24 years" + "~ahttps://goo.gl/photos/dxTN5bMiAVqE5gA89~" + "~c0~" + ";"
                     + "~l0~"
               + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~"
-                    + "www.MostlyNonFiction.com" + "~awww.mostlynonfiction.com~" + "~c0~" + ";"
+                    + "www.MostlyNonFiction.com" + "~ahttp://www.mostlynonfiction.com~" + "~c0~" + ";"
                     + "~l0~"
               + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~"
                     + "super-cool" + "~ahttps://goo.gl/photos/YKGpuzTFbfbqWzKz6~" + "~c0~" + ";"
