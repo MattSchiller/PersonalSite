31ea6ef5d21c3d705741c4ce0b4f1fa2064854b7
Matt Schiller <matt.s.schiller@gmail.com>
2016-05-13 00:00:59 -0400

    fixed hobbies

--- build/bundle.js
+++ build/bundle.js
@@ -20186,7 +20186,7 @@
 	var Hobbies = {
 	      stub: "~Cindent0~" + "~ccomment~/* So you're interested to know what sort of things I like to get up to in my free time, eh?" + "~l0~" + "~Cindent0~" + "~l0~" + "~Cindent0~" + "~ccomment~ Well, I'm happy to oblige! Feel free to explore the links below to take you to the relevant albums. */" + "~l0~" + "~Cindent0~" + "~l0~",
 
-	      writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~" + "24 years" + "~ahttps://goo.gl/photos/dxTN5bMiAVqE5gA89~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~" + "www.MostlyNonFiction.com" + "~awww.mostlynonfiction.com~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~" + "super-cool" + "~ahttps://goo.gl/photos/YKGpuzTFbfbqWzKz6~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
+	      writing: "~Cindent0~" + ".hobbies" + "~CfuncName~" + " {" + "~l0~" + "~Cindent1~" + "origami" + "~Cfunc~" + "~cspace~:     " + "~c0~" + "24 years" + "~ahttps://goo.gl/photos/dxTN5bMiAVqE5gA89~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "travel-blog" + "~Cfunc~" + ": " + "~c0~" + "www.MostlyNonFiction.com" + "~ahttp://www.mostlynonfiction.com~" + "~c0~" + ";" + "~l0~" + "~Cindent1~" + "costumes" + "~Cfunc~" + "~cspace~:    " + "~c0~" + "super-cool" + "~ahttps://goo.gl/photos/YKGpuzTFbfbqWzKz6~" + "~c0~" + ";" + "~l0~" + "~Cindent0~" + "}"
 	};
 
 	module.exports = Hobbies;
