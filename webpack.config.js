7673ea9d6dfbc57f73d1ba70f889027d6caf1915
Matt Schiller <matt.s.schiller@gmail.com>
2018-10-04 18:19:27 -0400

    Got the barebones of the site working again with an updated environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
-var webpack = require('webpack');
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: [
-    './src/scripts/main.jsx'
-  ],
-  output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test:/\.js?$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
-      }
-    ],
-  },
-  plugins: [
-    new webpack.NoErrorsPlugin()
-  ]
+module.exports = () => {
+    const config = {
+        entry: path.join(__dirname, "./src/scripts/main.jsx"),
+        output: {
+            path: __dirname + '/build',
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    loader: "babel-loader",
+                    exclude: /node_modules/,
+                }
+            ],
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+
+            new HtmlWebpackPlugin({
+                hash: true,
+                filename: "index.html",
+                template: __dirname + "/index.html",
+                favicon: __dirname + "/assets/images/favicon.ico"
+            })
+        ]
+    };
+
+    return config;
 };
