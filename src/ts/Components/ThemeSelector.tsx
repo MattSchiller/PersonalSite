d0a77b10ed27d114f71c4201b36371f0a60c429d
Matt Schiller <matt.s.schiller@gmail.com>
2019-02-22 15:06:59 -0500

    Added a light theme, capoLight, to the mix

--- src/ts/Components/ThemeSelector.tsx
+++ src/ts/Components/ThemeSelector.tsx
@@ -17,28 +17,58 @@ export class ThemeSelector extends React.PureComponent<any, IThemeSelectorState>
         this.state = {
             isSelected: false
         };
+
+        // TODO: Add listener for if the user clicks somewhere else to collapse this div.
     }
 
     public render() {
-        return (
+        return [
             <li
                 onClick={ this._toggleSelected }
-                className={ this._getClassName() }
+                className={ CSS.themeSelector + this._getSelectedClassName() }
             >
                 <img src={ themeIconUrl } />
                 Theme
-            </li >
-        );
-    }
-
-    private _getClassName(): string {
-        return `${CSS.themeSelector}
-            ${this.state.isSelected ? CSS.selected : ""}`;
+                { this._renderOptions() }
+            </li >,
+        ];
     }
 
     private _toggleSelected = () => {
         this.setState({
             isSelected: !this.state.isSelected
         });
+    }
+
+    private _renderOptions(): JSX.Element {
+        return (
+            <div className={ CSS.themeOptions } >
+                { Object.values(IThemesEnum).map(theme => <ThemeOption theme={ theme } />) }
+            </div>
+        );
+    }
+
+    private _getSelectedClassName(): string {
+        console.log("yoyoyoyoyo")
+        return ` ${this.state.isSelected ? CSS.selected : ""}`;
+    }
+}
+
+interface IThemeOptionProps {
+    theme: IThemesEnum;
+}
+
+export class ThemeOption extends React.PureComponent<IThemeOptionProps> {
+    public render() {
+        return (
+            <div onClick={ this._onClick }>
+                { this.props.theme }
+            </div>
+        )
+    }
+
+    private _onClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        event.stopPropagation();
+        Actions.setActiveTheme(this.props.theme);
     }
 }
--- src/ts/Helpers/Theming.ts
+++ src/ts/Helpers/Theming.ts
@@ -1,8 +1,8 @@
 export enum IThemesEnum {
-    SUBLIME_MONOKAI = "sublime-monokai",
-    OTHER = "other"
+    DARK_PLUS = "darkPlus",
+    CAPO_LIGHT = "capoLight"
 }
 
 export function getThemedClassName(className: string): string {
-    return `${className} ${className}-${IThemesEnum.SUBLIME_MONOKAI}`;
+    return `${className} ${className}-${IThemesEnum.CAPO_LIGHT}`;
 }
