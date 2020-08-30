d186fbdf32fb8729e9004a6b0137004e5c919b65
Matt Schiller <matt.s.schiller@gmail.com>
2020-08-30 13:49:24 -0400

    tweaks to history

--- src/ts/Components/MenuItem.tsx
+++ src/ts/Components/MenuItem.tsx
@@ -1,29 +1,25 @@
-import { Actions } from "@Redux/Actions";
+import { historyPush } from "@Helpers/History";
 import { IPage } from "@Redux/Interfaces/IStore";
 import CSS from "@Sass/styles.scss";
-import { history } from "@Helpers/History";
 import React from "react";
 
 interface IMenuItemProps extends IPage {
-    isSelected: boolean;
+  isSelected: boolean;
 }
 
 export class MenuItem extends React.PureComponent<IMenuItemProps> {
-    public render() {
-        return (
-            <li
-                onClick={ this._onClick }
-                className={ this._getClassName() }
-            >
-                <img src={ this.props.iconUrl } />
-                { this.props.pageTitle }
-            </li >
-        );
-    }
+  public render() {
+    return (
+      <li onClick={this._onClick} className={this._getClassName()}>
+        <img src={this.props.iconUrl} />
+        {this.props.pageTitle}
+      </li>
+    );
+  }
 
-    private _getClassName(): string {
-        return this.props.isSelected ? CSS.selected : "";
-    }
+  private _getClassName(): string {
+    return this.props.isSelected ? CSS.selected : "";
+  }
 
-    private _onClick = () => history.push(this.props.pageId);
+  private _onClick = () => historyPush(this.props.pageId);
 }
