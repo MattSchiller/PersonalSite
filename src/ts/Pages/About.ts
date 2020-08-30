7d8fd885f86d4ea5654aaa6fdabced434d7283a9
Matt Schiller <matt.s.schiller@gmail.com>
2020-08-30 13:41:40 -0400

    Re-updated some thing after repo clone

--- src/ts/Pages/About.ts
+++ src/ts/Pages/About.ts
@@ -1,52 +1,82 @@
 import { IRawPage } from "@Redux/Interfaces/IStore";
 
 export const About: IRawPage = {
-    pageId: "about",
-    pageTitle: "about.html",
-    iconUrl: "assets/images/htmlIcon.png",
-    language: "html",
-    simTypes: [{
-        simTypeId: "About_1",
-        sourceText:
-            // tslint:disable:max-line-length
-            "~s~" +
-            "~ccomment-fade~<!~ckey~DOCTYPE~ctint-default~ html~ccomment-fade~>" +
-            "~l~" +
-            "~l~" +
-            "~ccomment~<!-- Hi, I'm Matt Schiller, a Web Developer and freelance Technical Consultant." +
-            "~l~" +
-            "~l~" +
-            "~ccomment~(I build out enterprise software capabilities and construct cool things on the internet for fun.) -->" + "~l~" +
-            "~l~" +
-            "~s~" +
-
-            "~Cindent0 comment-fade~" + "<" + "~ckey~html" + "~ccomment-fade~>" + "~l~" +
-            "~p350~" + "~Cindent1 comment-fade~" + "<" + "~ckey~" + "body" + "~ccomment-fade~>" +
-            "~l~" +
-            "~p350~" + "~Cindent2~" + "My work experience grows from a SQL & Excel/VBA backgron" + "~p350~" + "~b1~" + "und that became a more comprehensive developer's toolkit (JS, C#). For years I'd implemented SaaS for Pharmacuetical" + "~p350~" + "~b7~" + "eutical, Healthcare, and Higher Education systems (like " +
-            "~l~" + "~cindent2~Johnson & Johnson" + "~ahttp://www.janssen.com/~" +
-            "~c~ and " +
-            "~c~Gonzaga University" + "~ahttp://www.gonzaga.org~" + "~c0~)" +
-            ", often having to learn new" + "~l~~cindent2~" + "languages and database structures in the process. Which is 100% my jam." +
-            "~l2~~l~" +
-
-            "~Cindent2~I thrive in using the right tool for a job, whether it's in my toolbelt yet or not. Sometimes that means the React/Redux frameworks in TypeScript to put together the dynamic website you see before you (" +
-            "~c0~github link~ahttps://github.com/MattSchiller/PersonalSite/~" + "~c0~). Sometimes it" + "~l~" +
-            "~cindent2~means learning how 3D modeling and rendering works to code while managing artists as co-founder/COO of a 3D platform startup, ~c~Strypes" + "~ahttps://app.box.com/v/StrypesDemoVideo/~" + "~c~." +
-            "~l2~~l~" +
-
-            "~Cindent2~" + "And a good day's work is great, going to bed at night feeling like you've really SPENT " +
-            "the day well is one of my favorite things, but there's more to life than a good work ethic. I believe in " +
-            "building fun and interesting things all the time, separate from my day job. For instance, I'm an avid folder of " +
-            "origami as well as a determined writer (both of which will be on this site shortly)." +
-            "~l2~~l~" +
-
-            "~Cindent2~" + "So welcome to my corner of the web!" +
-            "~l2~~l~" +
-
-            "~Cindent2~And thanks for checking out my site! Mchu" + "~b3~" + "uch of it is still under construcktoi" + "~p300~" + "~b4~" +
-            "tion." + "~l~" +
-            "~Cindent1 comment-fade~" + "</" + "~ckey~body" + "~ccomment-fade~>" + "~l~" +
-            "~Cindent0 comment-fade~" + "</" + "~ckey~html" + "~ccomment-fade~>" + "~l~"
-    }],
+  pageId: "about",
+  pageTitle: "about.html",
+  iconUrl: "assets/images/htmlIcon.png",
+  language: "html",
+  simTypes: [
+    {
+      simTypeId: "About_1",
+      sourceText:
+        // tslint:disable:max-line-length
+        "~s~" +
+        "~ccomment-fade~<!~ckey~DOCTYPE~ctint-default~ html~ccomment-fade~>" +
+        "~l~" +
+        "~l~" +
+        "~ccomment~<!-- Hi, I'm Matt Schiller, a Web Developer and freelance Technical Consultant." +
+        "~l~" +
+        "~l~" +
+        "~ccomment~(I build out enterprise software capabilities and construct cool things on the internet for fun.) -->" +
+        "~l~" +
+        "~l~" +
+        "~s~" +
+        "~Cindent0 comment-fade~" +
+        "<" +
+        "~ckey~html" +
+        "~ccomment-fade~>" +
+        "~l~" +
+        "~p350~" +
+        "~Cindent1 comment-fade~" +
+        "<" +
+        "~ckey~" +
+        "body" +
+        "~ccomment-fade~>" +
+        "~l~" +
+        "~p350~" +
+        "~Cindent2~" +
+        "My work experience grows from a SQL & Excel/VBA consulting backgron" +
+        "~p350~" +
+        "~b1~" +
+        "und that grew to include a more comprehensive developer's toolkit (JS/TS, C#). For years I'd implemented SaaS for Pharmacuetical" +
+        "~p350~" +
+        "~b7~" +
+        "eutical, Healthcare, and Higher Education systems, often having to learn new languages and database structures in the process. Which is 100% my jam." +
+        "~l2~~l~" +
+        "~Cindent2~I thrive in using the right tool for a job, whether it's in my toolbelt yet or not. Sometimes that means the React/Redux frameworks in TypeScript to put together the dynamic website you see before you (" +
+        "~c0~github link~ahttps://github.com/MattSchiller/PersonalSite/~" +
+        "~c0~). Sometimes it" +
+        "~l~" +
+        "~cindent2~means learning how 3D modeling and rendering works, to code while managing artists as co-founder/COO of a 3D platform-for-fashion startup, ~c~Strypes" +
+        "~ahttps://app.box.com/v/StrypesDemoVideo/~" +
+        "~c~." +
+        "~l2~~l~" +
+        "~Cindent2~" +
+        "These days I'm a web developer at ~c~Tumblr~ahttps://www.tumblr.com/~" +
+        "~c~. " +
+        "I like to solve interesting " +
+        "~l~~cindent2~" +
+        "problems and put myself in positions where I feel like I know nothing compared to the people I'm surrounded by. That's the best way to learn and to grow, and that's what I'm here for." +
+        "~l2~~l~" +
+        "~Cindent2~" +
+        "And a good day's work is great, going to bed at night feeling like you've really SPENT " +
+        "the day well is one of my favorite things, but there's more to life than a good work ethic. I believe in " +
+        "building fun and interesting things all the time, separate from my day job. For instance, I'm an avid " +
+        "folder of origami and a hobbyist trivia-blog writer." +
+        "~l2~~l~" +
+        "~Cindent2~" +
+        "So welcome to my little web presence!" +
+        "~l~" +
+        "~Cindent1 comment-fade~" +
+        "</" +
+        "~ckey~body" +
+        "~ccomment-fade~>" +
+        "~l~" +
+        "~Cindent0 comment-fade~" +
+        "</" +
+        "~ckey~html" +
+        "~ccomment-fade~>" +
+        "~l~",
+    },
+  ],
 };
